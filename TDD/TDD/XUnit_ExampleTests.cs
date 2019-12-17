ec54fb7dc4265596c1f60e04cf3159c0be25df8c
Jakub <jakub.szymczak93@gmail.com>
2019-12-17 10:37:23 +0100

    First Test

---
+++ TDD/TDD/XUnit_ExampleTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TDD
+{
+    public class XUnit_ExampleTests : IDisposable
+    {
+
+        [Fact]
+        public void Unit_test_example()
+        {
+            //arrange
+
+            //act
+
+            //assert
+        }
+        public void Dispose()
+        {
+        }
+    }
+}
