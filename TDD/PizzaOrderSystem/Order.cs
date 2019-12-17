e698422211e3318e595e104f78a49c502e772946
Jakub <jakub.szymczak93@gmail.com>
2019-12-17 14:25:34 +0100

    Pizza tests implementation

--- TDD/PizzaOrderSystem/Order.cs
+++ TDD/PizzaOrderSystem/Order.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PizzaOrderSystem
 {
     internal class Order
     {
+        private IList<OrderPosition> _positions = new List<OrderPosition>();
 
         public Order()
         {
@@ -11,11 +14,18 @@ namespace PizzaOrderSystem
 
         internal void Add(OrderPosition orderPosition)
         {
+            _positions.Add(orderPosition);
         }
 
         internal bool IsValid()
         {
-            return true;
+            return _positions.Sum(p => p.Pieces) % 8 == 0 && _positions.GroupBy(p => p.Name)
+                             .Select(p => new
+                             {
+                                 PizzaName = p.Key,
+                                 TotalPieces = p.Sum(x => x.Pieces)
+                             })
+                             .All(g => g.TotalPieces % 4 == 0);
         }
     }
 }
--- TDD/PizzaOrderSystem/PizzaTests.cs
+++ TDD/PizzaOrderSystem/PizzaTests.cs
@@ -9,13 +9,51 @@ namespace PizzaOrderSystem
 {
     public class PizzaTests
     {
+        private readonly Order _order; 
+        public PizzaTests()
+        {
+            _order = new Order();
+        }
+
         [Fact]
         public void When_one_person_orders_8_pieces_then_order_is_valid() 
         {
-            var order = new Order();
+            _order.Add(new OrderPosition("Arek", 8 , "4 sery"));
+            Assert.True(_order.IsValid());
+        }
 
-            order.Add(new OrderPosition("Arek", 8 , "4 sery"));
-            Assert.True(order.IsValid());
+        [Fact]
+        public void When_sum_of_pieces_is_not_divisible_by_8_order_is_no_valid()
+        {
+
+            _order.Add(new OrderPosition("Arek", 3, "4 sery"));
+            _order.Add(new OrderPosition("Arek", 4, "4 sery"));
+            Assert.False(_order.IsValid());
+        }
+
+        [Fact]
+        public void When_two_people_order_total_8_pieces__of_different_pizza_and_not_a_half_then_order_is_invalid()
+        {
+            _order.Add(new OrderPosition("Arek", 3, "4 sery"));
+            _order.Add(new OrderPosition("Ola", 5, "poznanska"));
+            Assert.False(_order.IsValid());
+        }
+
+        [Fact]
+        public void When_two_people_order_four_pieces_of_different_pizza_then_order_is_invalid()
+        {
+            _order.Add(new OrderPosition("Arek", 4, "4 sery"));
+            _order.Add(new OrderPosition("Ola", 4, "poznanska"));
+            Assert.True(_order.IsValid());
+        }
+
+        [Fact]
+        public void When_three_people_order_four_pieces_of_different_pizzas_each_other_is_invalid()
+        {
+            _order.Add(new OrderPosition("Arek", 4, "4 sery"));
+            _order.Add(new OrderPosition("Ola", 4, "poznanska"));
+            _order.Add(new OrderPosition("Jarek", 4, "poznanska"));
+            Assert.False(_order.IsValid());
         }
     }
 }
